533768aa49187a76580993e92bb4ef18ac7e96a7
RoshandeepSingh28 <155190705+RoshandeepSingh28@users.noreply.github.com>
2024-09-19 03:43:38 +0530

    Add files via upload

---
+++ PostSelection.jsx
@@ -0,0 +1,23 @@
+function PostSelection({checkState}) {
+    const handleOptionChange = (event) => {
+      const selectedOption = event.target.value;
+      checkState(selectedOption);  
+    };
+    return(
+        <div>
+            <header>
+                <h3>New Post</h3>
+                <div>
+                <p>Select Post Type: </p>
+                <input type="radio" id="Question" 
+                    name="option" value="question" defaultChecked onChange={handleOptionChange} />
+                <label htmlFor="Question">Question</label>
+                <input type="radio" id="Article" 
+                    name="option" value="article" onChange={handleOptionChange} />
+                <label htmlFor="Article">Article</label>
+                </div>
+                </header>
+        </div>
+    )
+}
+export default PostSelection
